6a2300c8ed4af8f455b9b08d4069fa7c32e9a3ab
Trololol ololol <jschafli@gmail.com>
2011-10-11 13:58:11 +1300

    Add Sponsors.lua file

---
+++ SoundAlerter/sponsors.lua
@@ -0,0 +1,26 @@
+SA_sponsors = {
+	["A"] = "Official Developer of SoundAlerter",
+	["B"] = "Official Sponsor of SoundAlerter",
+	["C"] = "Official Contributer of SoundAlerter",
+--devs
+	--Warsong
+	["Trollolloll"] = { ["Realm"] = "Warsong (Pure PvP)", ["Type"] = "A" },
+	--Sargeras
+	["Lockmepls"] = { ["Realm"] = "Sargeras x20", ["Type"] = "A" },
+	["Trollollollo"] = { ["Realm"] = "Sargeras x20", ["Type"] = "A" },
+	["Trollololool"] = { ["Realm"] = "Sargeras x20", ["Type"] = "A" },
+	["Ammonia"] = { ["Realm"] = "Sargeras x20", ["Type"] = "A" },
+	["Trolollolol"] = { ["Realm"] = "Sargeras x20", ["Type"] = "A" },
+	
+--contributers
+	--Warsong
+	["Shaquetta	"] = { ["Realm"] = "Warsong (Pure PvP)", ["Type"] = "C" },
+	["Hotslock	"] = { ["Realm"] = "Warsong (Pure PvP)", ["Type"] = "C" },
+	--Neltharion
+	["Drterror"] = { ["Realm"] = "Neltharion x12", ["Type"] = "C" },
+	["Horrorshaman"] = { ["Realm"] = "Neltharion x12", ["Type"] = "C" },
+	["Cheaptrick"] = { ["Realm"] = "Neltharion x12", ["Type"] = "C" },
+	["Zerodeath"] = { ["Realm"] = "Neltharion x12", ["Type"] = "C" },
+	--Frostwolf
+	["Clov"] = { ["Realm"] = "Frostwolf x3", ["Type"] = "C" }
+	}
