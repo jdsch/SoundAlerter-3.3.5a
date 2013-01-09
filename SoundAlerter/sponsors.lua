acc793884d83e45959808de8201e50257c711f10
trolololol <trolollolol@trolololol.com>
2013-01-09 20:00:40 +1300

    Add 2 contribs

--- SoundAlerter/sponsors.lua
+++ SoundAlerter/sponsors.lua
@@ -17,5 +17,7 @@ SA_sponsors = {
 	--Deathwing
 	["Jeuriess"] = { ["Realm"] = "Deathwing x12", ["Type"] = "C" },
 	["Sanathas"] = { ["Realm"] = "Deathwing x12", ["Type"] = "C" },
+	["Natasi"] = { ["Realm"] = "Deathwing x12", ["Type"] = "C" },
+	["Aloysius"] = { ["Realm"] = "Deathwing x12", ["Type"] = "C" },
 	["Kayrop"] = { ["Realm"] = "Ragnaros x20", ["Type"] = "C" }
 	}
